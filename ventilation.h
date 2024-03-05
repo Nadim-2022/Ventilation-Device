f3fccb49a9d720fcf635b350e1f4233cb36ce4a2
Nadim-2022 <i.am.nadim95@gmail.com>
2024-03-05 14:04:59 +0200

    Main class has been created

---
+++ ventilation.h
@@ -0,0 +1,14 @@
+//
+// Created by iamna on 05/03/2024.
+//
+
+#ifndef VENTILATION_DEVICE_VENTILATION_H
+#define VENTILATION_DEVICE_VENTILATION_H
+
+
+class ventilation {
+
+};
+
+
+#endif //VENTILATION_DEVICE_VENTILATION_H
